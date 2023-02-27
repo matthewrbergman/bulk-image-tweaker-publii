bfb1c27a2c4279fb60a19e618d735eb570bc4df6
matthewrbergman <126120292+matthewrbergman@users.noreply.github.com>
2023-02-27 09:06:59 -0600

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,12 @@
+class BulkImageTweaker {
+	constructor (API, name, config) {
+		this.API = API;
+		this.name = name;
+		this.config = config;
+	}
+
+	// TODO
+}
+
+
+module.exports = BulkImageTweaker;
